26f31eb8a5173ab0d493abdc6f2c56380b4a3831
Mbogo-anonymous <mbogoalex3@gmail.com>
2024-08-20 17:58:45 +0300

    update version - 89

--- src/main/java/com/app/CashLedger/services/AppVersionServiceImpl.java
+++ src/main/java/com/app/CashLedger/services/AppVersionServiceImpl.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class AppVersionServiceImpl implements AppVersionService {
     @Override
     public Double getCurrentAppVersion() {
-        return 88.0;
+        return 89.0;
     }
 }
